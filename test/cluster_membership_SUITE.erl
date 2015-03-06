fd426d5495248047c5c6801f562823b17ce5f394
Andrew Thompson <andrew@hijacked.us>
2015-03-06 17:24:12 +0000

    Refactor helper code into common module, more pmap, new rebar

--- test/cluster_membership_SUITE.erl
+++ test/cluster_membership_SUITE.erl
@@ -44,14 +44,14 @@ end_per_suite(_Config) ->
     _Config.
 
 init_per_testcase(Case, Config) ->
-    Nodes = pmap(fun(N) ->
-                    start_node(N, Case, true)
+    Nodes = plumtree_test_utils:pmap(fun(N) ->
+                    plumtree_test_utils:start_node(N, Config, Case)
             end, [jaguar, shadow, thorn, pyros]),
     {ok, _} = ct_cover:add_nodes(Nodes),
     [{nodes, Nodes}|Config].
 
 end_per_testcase(_, _Config) ->
-    pmap(fun(Node) ->ct_slave:stop(Node) end, [jaguar, shadow, thorn, pyros]),
+    plumtree_test_utils:pmap(fun(Node) ->ct_slave:stop(Node) end, [jaguar, shadow, thorn, pyros]),
     ok.
 
 all() ->
@@ -61,32 +61,32 @@ all() ->
 singleton_test(Config) ->
     Nodes = proplists:get_value(nodes, Config),
     ok = ct_cover:remove_nodes(Nodes),
-    [[Node] = get_cluster_members(Node) || Node <- Nodes],
+    [[Node] = plumtree_test_utils:get_cluster_members(Node) || Node <- Nodes],
     ok.
 
 join_test(Config) ->
     [Node1, Node2 |Nodes] = proplists:get_value(nodes, Config),
     ?assertEqual(ok, rpc:call(Node1, plumtree_peer_service, join, [Node2])),
     Expected = lists:sort([Node1, Node2]),
-    ok = wait_until_joined([Node1, Node2], Expected),
-    ?assertEqual(Expected, lists:sort(get_cluster_members(Node1))),
-    ?assertEqual(Expected, lists:sort(get_cluster_members(Node2))),
+    ok = plumtree_test_utils:wait_until_joined([Node1, Node2], Expected),
+    ?assertEqual(Expected, lists:sort(plumtree_test_utils:get_cluster_members(Node1))),
+    ?assertEqual(Expected, lists:sort(plumtree_test_utils:get_cluster_members(Node2))),
     %% make sure the last 2 are still singletons
-    [?assertEqual([Node], get_cluster_members(Node)) || Node <- Nodes],
+    [?assertEqual([Node], plumtree_test_utils:get_cluster_members(Node)) || Node <- Nodes],
     ok.
 
 join_nonexistant_node_test(Config) ->
     [Node1|_] = proplists:get_value(nodes, Config),
     ?assertEqual({error, not_reachable}, rpc:call(Node1, plumtree_peer_service, join,
                               [fake@fakehost])),
-    ?assertEqual([Node1], get_cluster_members(Node1)),
+    ?assertEqual([Node1], plumtree_test_utils:get_cluster_members(Node1)),
     ok.
 
 join_self_test(Config) ->
     [Node1|_] = proplists:get_value(nodes, Config),
     ?assertEqual({error, self_join}, rpc:call(Node1, plumtree_peer_service, join,
                               [Node1])),
-    ?assertEqual([Node1], get_cluster_members(Node1)),
+    ?assertEqual([Node1], plumtree_test_utils:get_cluster_members(Node1)),
     ok.
 
 leave_test(Config) ->
@@ -94,16 +94,16 @@ leave_test(Config) ->
     [?assertEqual(ok, rpc:call(Node, plumtree_peer_service, join, [Node1]))
      || Node <- OtherNodes],
     Expected = lists:sort(Nodes),
-    ok = wait_until_joined(Nodes, Expected),
+    ok = plumtree_test_utils:wait_until_joined(Nodes, Expected),
     [?assertEqual({Node, Expected}, {Node,
-                                     lists:sort(get_cluster_members(Node))})
+                                     lists:sort(plumtree_test_utils:get_cluster_members(Node))})
      || Node <- Nodes],
     ?assertEqual(ok, rpc:call(Node1, plumtree_peer_service, leave, [])),
     Expected2 = lists:sort(OtherNodes),
-    ok = wait_until_left(OtherNodes, Node1),
+    ok = plumtree_test_utils:wait_until_left(OtherNodes, Node1),
     %% should be a 3 node cluster now
     [?assertEqual({Node, Expected2}, {Node,
-                                      lists:sort(get_cluster_members(Node))})
+                                      lists:sort(plumtree_test_utils:get_cluster_members(Node))})
      || Node <- OtherNodes],
     %% node1 should be offline
     ?assertEqual(pang, net_adm:ping(Node1)),
@@ -114,36 +114,36 @@ sticky_membership_test(Config) ->
     [?assertEqual(ok, rpc:call(Node, plumtree_peer_service, join, [Node1]))
      || Node <- OtherNodes],
     Expected = lists:sort(Nodes),
-    ok = wait_until_joined(Nodes, Expected),
+    ok = plumtree_test_utils:wait_until_joined(Nodes, Expected),
     [?assertEqual({Node, Expected}, {Node,
-                                     lists:sort(get_cluster_members(Node))})
+                                     lists:sort(plumtree_test_utils:get_cluster_members(Node))})
      || Node <- Nodes],
     ct_slave:stop(jaguar),
-    ok = wait_until_offline(Node1),
+    ok = plumtree_test_utils:wait_until_offline(Node1),
     %% check the membership is the same
     [?assertEqual({Node, Expected}, {Node,
-                                     lists:sort(get_cluster_members(Node))})
+                                     lists:sort(plumtree_test_utils:get_cluster_members(Node))})
      || Node <- OtherNodes],
-    start_node(jaguar, sticky_membership_test, false),
+    plumtree_test_utils:start_node(jaguar, Config, sticky_membership_test),
     ?assertEqual({Node1, Expected}, {Node1,
-                                    lists:sort(get_cluster_members(Node1))}),
+                                    lists:sort(plumtree_test_utils:get_cluster_members(Node1))}),
     ct_slave:stop(jaguar),
-    ok = wait_until_offline(Node1),
+    ok = plumtree_test_utils:wait_until_offline(Node1),
     [Node2|LastTwo] = OtherNodes,
     ?assertEqual(ok, rpc:call(Node2, plumtree_peer_service, leave, [])),
-    ok = wait_until_left(LastTwo, Node2),
-    ok = wait_until_offline(Node2),
+    ok = plumtree_test_utils:wait_until_left(LastTwo, Node2),
+    ok = plumtree_test_utils:wait_until_offline(Node2),
     Expected2 = lists:sort(Nodes -- [Node2]),
     [?assertEqual({Node, Expected2}, {Node,
-                                      lists:sort(get_cluster_members(Node))})
+                                      lists:sort(plumtree_test_utils:get_cluster_members(Node))})
      || Node <- LastTwo],
-    start_node(jaguar, sticky_membership_test, false),
-    ok = wait_until_left([Node1], Node2),
+    plumtree_test_utils:start_node(jaguar, Config, sticky_membership_test),
+    ok = plumtree_test_utils:wait_until_left([Node1], Node2),
     ?assertEqual({Node1, Expected2}, {Node1,
-                                    lists:sort(get_cluster_members(Node1))}),
-    start_node(shadow, sticky_membership_test, false),
+                                    lists:sort(plumtree_test_utils:get_cluster_members(Node1))}),
+    plumtree_test_utils:start_node(shadow, Config, sticky_membership_test),
     %% node 2 should be a singleton now
-    ?assertEqual([Node2], get_cluster_members(Node2)),
+    ?assertEqual([Node2], plumtree_test_utils:get_cluster_members(Node2)),
     ok.
 
 
@@ -151,103 +151,3 @@ sticky_membership_test(Config) ->
 %% utility functions
 %% ===================================================================
 
-get_cluster_members(Node) ->
-    {Node, {ok, Res}} = {Node, rpc:call(Node, plumtree_peer_service_manager, get_local_state, [])},
-    riak_dt_orswot:value(Res).
-
-pmap(F, L) ->
-    Parent = self(),
-    lists:foldl(
-        fun(X, N) ->
-                spawn_link(fun() ->
-                            Parent ! {pmap, N, F(X)}
-                    end),
-                N+1
-        end, 0, L),
-    L2 = [receive {pmap, N, R} -> {N,R} end || _ <- L],
-    {_, L3} = lists:unzip(lists:keysort(1, L2)),
-    L3.
-
-wait_until(Fun, Retry, Delay) when Retry > 0 ->
-    Res = Fun(),
-    case Res of
-        true ->
-            ok;
-        _ when Retry == 1 ->
-            {fail, Res};
-        _ ->
-            timer:sleep(Delay),
-            wait_until(Fun, Retry-1, Delay)
-    end.
-
-wait_until_left(Nodes, LeavingNode) ->
-    %ct:pal("waiting for ~p to leave ~p", [LeavingNode, Nodes]),
-    wait_until(fun() ->
-                lists:all(fun(X) -> X == true end, [begin
-                            %ct:pal("Node ~p - ~p -- ~p = ~p", [Node,
-                                                          %get_cluster_members(Node),
-                                                          %LeavingNode, not
-                                                               %lists:member(LeavingNode,
-                                                                            %get_cluster_members(Node))]),
-                            not
-                            lists:member(LeavingNode,
-                                         get_cluster_members(Node))
-                        end
-                            || Node <-
-                            Nodes])
-        end, 60*2, 500).
-
-wait_until_joined(Nodes, ExpectedCluster) ->
-    wait_until(fun() ->
-                lists:all(fun(X) -> X == true end, [
-                        lists:sort(ExpectedCluster) ==
-                        lists:sort(get_cluster_members(Node))
-                        || Node <-
-                           Nodes])
-        end, 60*2, 500).
-
-wait_until_offline(Node) ->
-    wait_until(fun() ->
-                pang == net_adm:ping(Node)
-        end, 60*2, 500).
-
-start_node(Name, Case, Clean) ->
-    CodePath = lists:filter(fun filelib:is_dir/1, code:get_path()),
-    %% have the slave nodes monitor the runner node, so they can't outlive it
-    NodeConfig = [
-            {monitor_master, true},
-            {erl_flags, "-smp"}, %% smp for the eleveldb god
-            {startup_functions, [
-                    {code, set_path, [CodePath]}
-                    ]}],
-    case ct_slave:start(Name, NodeConfig) of
-        {ok, Node} ->
-            NodeDir = filename:join(["/tmp", Node, Case]),
-            case Clean of
-                true ->
-                    %% yolo, if this deletes your HDD, I'm sorry...
-                    os:cmd("rm -rf "++NodeDir);
-                _ ->
-                    ok
-            end,
-            ok = rpc:call(Node, application, load, [plumtree]),
-            ok = rpc:call(Node, application, load, [lager]),
-            ok = rpc:call(Node, application, set_env, [lager,
-                                                       log_root,
-                                                       NodeDir]),
-            ok = rpc:call(Node, application, set_env, [plumtree,
-                                                       plumtree_data_dir,
-                                                       NodeDir]),
-            {ok, _} = rpc:call(Node, application, ensure_all_started, [plumtree]),
-            ok = wait_until(fun() ->
-                            case rpc:call(Node, plumtree_peer_service_manager, get_local_state, []) of
-                                {ok, _Res} -> true;
-                                _ -> false
-                            end
-                    end, 60, 500),
-            Node;
-        {error, already_started, Node} ->
-            ct_slave:stop(Name),
-            wait_until_offline(Node),
-            start_node(Name, Case, Clean)
-    end.
--- test/metadata_SUITE.erl
+++ test/metadata_SUITE.erl
@@ -40,14 +40,14 @@ end_per_suite(_Config) ->
     _Config.
 
 init_per_testcase(Case, Config) ->
-    Nodes = pmap(fun(N) ->
-                    start_node(N, Case, true)
+    Nodes = plumtree_test_utils:pmap(fun(N) ->
+                    plumtree_test_utils:start_node(N, Config, Case)
             end, [electra, katana, flail, gargoyle]),
     {ok, _} = ct_cover:add_nodes(Nodes),
     [{nodes, Nodes}|Config].
 
 end_per_testcase(_, _Config) ->
-    pmap(fun(Node) ->ct_slave:stop(Node) end, [electra, katana, flail, gargoyle]),
+    plumtree_test_utils:pmap(fun(Node) ->ct_slave:stop(Node) end, [electra, katana, flail, gargoyle]),
     ok.
 
 all() ->
@@ -58,9 +58,9 @@ read_write_delete_test(Config) ->
     [?assertEqual(ok, rpc:call(Node, plumtree_peer_service, join, [Node1]))
      || Node <- OtherNodes],
     Expected = lists:sort(Nodes),
-    ok = wait_until_joined(Nodes, Expected),
+    ok = plumtree_test_utils:wait_until_joined(Nodes, Expected),
     [?assertEqual({Node, Expected}, {Node,
-                                     lists:sort(get_cluster_members(Node))})
+                                     lists:sort(plumtree_test_utils:get_cluster_members(Node))})
      || Node <- Nodes],
     ?assertEqual(undefined, get_metadata(Node1, {foo, bar}, baz, [])),
     ok = put_metadata(Node1, {foo, bar}, baz, quux, []),
@@ -77,22 +77,22 @@ partitioned_cluster_test(Config) ->
     [?assertEqual(ok, rpc:call(Node, plumtree_peer_service, join, [Node1]))
      || Node <- OtherNodes],
     Expected = lists:sort(Nodes),
-    ok = wait_until_joined(Nodes, Expected),
+    ok = plumtree_test_utils:wait_until_joined(Nodes, Expected),
     [?assertEqual({Node, Expected}, {Node,
-                                     lists:sort(get_cluster_members(Node))})
+                                     lists:sort(plumtree_test_utils:get_cluster_members(Node))})
      || Node <- Nodes],
     ok = wait_until_converged(Nodes, {foo, bar}, baz, undefined),
     ok = put_metadata(Node1, {foo, bar}, baz, quux, []),
     ok = wait_until_converged(Nodes, {foo, bar}, baz, quux),
     {ANodes, BNodes} = lists:split(2, Nodes),
-    partition_cluster(ANodes, BNodes),
+    plumtree_test_utils:partition_cluster(ANodes, BNodes),
     %% write to one side
     ok = put_metadata(Node1, {foo, bar}, baz, norf, []),
     %% check that whole side has the new value
     ok = wait_until_converged(ANodes, {foo, bar}, baz, norf),
     %% the far side should have the old value
     ok = wait_until_converged(BNodes, {foo, bar}, baz, quux),
-    heal_cluster(ANodes, BNodes),
+    plumtree_test_utils:heal_cluster(ANodes, BNodes),
     %% all the nodes should see the new value
     ok = wait_until_converged(Nodes, {foo, bar}, baz, norf),
     ok.
@@ -110,136 +110,11 @@ put_metadata(Node, Prefix, Key, ValueOrFun, Opts) ->
 delete_metadata(Node, Prefix, Key) ->
     rpc:call(Node, plumtree_metadata, delete, [Prefix, Key]).
 
-get_cluster_members(Node) ->
-    {Node, {ok, Res}} = {Node, rpc:call(Node, plumtree_peer_service_manager, get_local_state, [])},
-    riak_dt_orswot:value(Res).
-
-pmap(F, L) ->
-    Parent = self(),
-    lists:foldl(
-        fun(X, N) ->
-                spawn_link(fun() ->
-                            Parent ! {pmap, N, F(X)}
-                    end),
-                N+1
-        end, 0, L),
-    L2 = [receive {pmap, N, R} -> {N,R} end || _ <- L],
-    {_, L3} = lists:unzip(lists:keysort(1, L2)),
-    L3.
-
-wait_until(Fun, Retry, Delay) when Retry > 0 ->
-    Res = Fun(),
-    case Res of
-        true ->
-            ok;
-        _ when Retry == 1 ->
-            {fail, Res};
-        _ ->
-            timer:sleep(Delay),
-            wait_until(Fun, Retry-1, Delay)
-    end.
-
-%wait_until_left(Nodes, LeavingNode) ->
-    %wait_until(fun() ->
-                %lists:all(fun(X) -> X == true end, [begin
-                            %not
-                            %lists:member(LeavingNode,
-                                         %get_cluster_members(Node))
-                        %end
-                            %|| Node <-
-                            %Nodes])
-        %end, 60*2, 500).
-
-wait_until_joined(Nodes, ExpectedCluster) ->
-    wait_until(fun() ->
-                lists:all(fun(X) -> X == true end, [
-                        lists:sort(ExpectedCluster) ==
-                        lists:sort(get_cluster_members(Node))
-                        || Node <-
-                           Nodes])
-        end, 60*2, 500).
-
-wait_until_offline(Node) ->
-    wait_until(fun() ->
-                pang == net_adm:ping(Node)
-        end, 60*2, 500).
-
-wait_until_disconnected(Node1, Node2) ->
-    wait_until(fun() ->
-                pang == rpc:call(Node1, net_adm, ping, [Node2])
-        end, 60*2, 500).
-
-wait_until_connected(Node1, Node2) ->
-    wait_until(fun() ->
-                pong == rpc:call(Node1, net_adm, ping, [Node2])
-        end, 60*2, 500).
-
 wait_until_converged(Nodes, Prefix, Key, ExpectedValue) ->
-    wait_until(fun() ->
-                lists:all(fun(X) -> X == true end, [begin
-                            ExpectedValue == get_metadata(Node, Prefix, Key,
-                                                          [])
-                        end
-                            || Node <-
-                            Nodes])
+    plumtree_test_utils:wait_until(fun() ->
+                lists:all(fun(X) -> X == true end,
+                          plumtree_test_utils:pmap(fun(Node) ->
+                                ExpectedValue == get_metadata(Node, Prefix, Key, [])
+                        end, Nodes))
         end, 60*2, 500).
-
-start_node(Name, Case, Clean) ->
-    CodePath = lists:filter(fun filelib:is_dir/1, code:get_path()),
-    %% have the slave nodes monitor the runner node, so they can't outlive it
-    NodeConfig = [
-            {monitor_master, true},
-            {erl_flags, "-smp"}, %% smp for the eleveldb god
-            {startup_functions, [
-                    {code, set_path, [CodePath]}
-                    ]}],
-    case ct_slave:start(Name, NodeConfig) of
-        {ok, Node} ->
-            NodeDir = filename:join(["/tmp", Node, Case]),
-            case Clean of
-                true ->
-                    %% yolo, if this deletes your HDD, I'm sorry...
-                    os:cmd("rm -rf "++NodeDir);
-                _ ->
-                    ok
-            end,
-            ok = rpc:call(Node, application, load, [plumtree]),
-            ok = rpc:call(Node, application, load, [lager]),
-            ok = rpc:call(Node, application, set_env, [lager,
-                                                       log_root,
-                                                       NodeDir]),
-            ok = rpc:call(Node, application, set_env, [plumtree,
-                                                       plumtree_data_dir,
-                                                       NodeDir]),
-            {ok, _} = rpc:call(Node, application, ensure_all_started, [plumtree]),
-            ok = wait_until(fun() ->
-                            case rpc:call(Node, plumtree_peer_service_manager, get_local_state, []) of
-                                {ok, _Res} -> true;
-                                _ -> false
-                            end
-                    end, 60, 500),
-            Node;
-        {error, already_started, Node} ->
-            ct_slave:stop(Name),
-            wait_until_offline(Node),
-            start_node(Name, Case, Clean)
-    end.
-
-partition_cluster(ANodes, BNodes) ->
-    [begin
-            true = rpc:call(Node1, erlang, set_cookie, [Node2, canttouchthis]),
-            true = rpc:call(Node1, erlang, disconnect_node, [Node2]),
-            ok = wait_until_disconnected(Node1, Node2)
-        end
-     || Node1 <- ANodes, Node2 <- BNodes],
-    ok.
-
-heal_cluster(ANodes, BNodes) ->
-    GoodCookie = erlang:get_cookie(),
-    [begin
-            true = rpc:call(Node1, erlang, set_cookie, [Node2, GoodCookie]),
-            ok = wait_until_connected(Node1, Node2)
-        end
-     || Node1 <- ANodes, Node2 <- BNodes],
-    ok.
 
